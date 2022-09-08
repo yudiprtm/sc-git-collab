8b90cfc263ca50fee54c019315a9bf53fed2241f
yudiprtm <yudifrank@gmail.com>
2022-09-08 20:13:26 +0700

    create main.py and gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+
+    data_1 = int(input("masukkan input 1 :"))
+    data_2 = int(input("masukkan input 2 :"))
+    operator = input("masukkan operator :")
+
+    print("{} {} {}".format(data_1, operator, data_2))
+
+
+if __name__ == "__main__":
+    print("Hello Main !")
+    main()
